0a26f728df8b3e86f4f71f0caac2964007441c81
Irving9910 <muniz.jair@live.com.mx>
2021-08-12 20:04:33 -0500

    first commit

---
+++ src/app/counter/counter/counter.component.ts
@@ -0,0 +1,32 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-counter',
+  template: `
+    <h1>{{ title }}</h1>
+    <h3>
+      The base is: <strong>{{ base }}</strong>
+    </h3>
+
+    <button (click)="accumulate(base)">+ {{ base }}</button>
+
+    <span>{{ number }}</span>
+
+    <button (click)="accumulate(-base)">- {{ base }}</button>
+
+    <button (click)="reset()">reset</button>
+  `,
+})
+export class CounterComponent {
+  title: string = 'Contador App';
+  number: number = 0;
+  base: number = 5;
+
+  accumulate(value: number) {
+    this.number += value;
+  }
+
+  reset() {
+    this.number = 0;
+  }
+}
